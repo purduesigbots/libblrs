9c2f155c542f84eb6e3bc6cc0775238f7e458605
Jonathan Bayless <baylessj@purdue.edu>
2017-10-13 13:17:40 -0400

    Accidentally forgot fbc_pid formatting

--- libfbc/src/fbc_pid.c
+++ libfbc/src/fbc_pid.c
@@ -13,35 +13,35 @@
 #include "fbc_pid.h"
 
 static int _pidCompute(fbc_t* fbc, int error) {
-  fbc_pid_t* data = (fbc_pid_t*)(fbc->_controllerData);
+	fbc_pid_t* data = (fbc_pid_t*)(fbc->_controllerData);
 
-  data->_integral += error;
-  if(data->_integral < data->minI)
-    data->_integral = data->minI;
-  else if(data->_integral > data->maxI)
-    data->_integral = data->maxI;
-  double derivative = ((double)(error - data->_prevError) / (CUR_TIME() - fbc->_prevExecution));
-  data->_prevError = error;
-  return (data->kP * error) + (data->kI * data->_integral) + (data->kD * derivative);
+	data->_integral += error;
+  	if(data->_integral < data->minI)
+    		data->_integral = data->minI;
+  	else if(data->_integral > data->maxI)
+    		data->_integral = data->maxI;
+  	double derivative = ((double)(error - data->_prevError) / (CUR_TIME() - fbc->_prevExecution));
+  	data->_prevError = error;
+  	return (data->kP * error) + (data->kI * data->_integral) + (data->kD * derivative);
 }
 
 static void _pidReset(fbc_t* fbc) {
-  fbc_pid_t * data = (fbc_pid_t*)(fbc->_controllerData);
-  data->_integral = 0;
-  data->_prevError = 0;
+	fbc_pid_t * data = (fbc_pid_t*)(fbc->_controllerData);
+  	data->_integral = 0;
+  	data->_prevError = 0;
 }
 
 void fbcPIDInitializeData(fbc_pid_t* fbc_pid, double kP, double kI, double kD,
-  int minIntegral, int maxIntegral) {
-  fbc_pid->kP = kP;
-  fbc_pid->kI = kI;
-  fbc_pid->kD = kD;
-  fbc_pid->maxI = maxIntegral;
-  fbc_pid->minI = minIntegral;
+                          int minIntegral, int maxIntegral) {
+	fbc_pid->kP = kP;
+	fbc_pid->kI = kI;
+	fbc_pid->kD = kD;
+	fbc_pid->maxI = maxIntegral;
+	fbc_pid->minI = minIntegral;
 }
 
 void fbcPIDInit(fbc_t* fbc, fbc_pid_t* config) {
-  fbc->compute = &_pidCompute;
-  fbc->_controllerData = config;
-  fbc->resetController = &_pidReset;
+	fbc->compute = &_pidCompute;
+	fbc->_controllerData = config;
+	fbc->resetController = &_pidReset;
 }
